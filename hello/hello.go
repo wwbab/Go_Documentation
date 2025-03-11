612455e57fcd8ea0356355562fb9a875f14f7e61
wuwenbin <3303274053@qq.com>
2025-03-11 10:51:44 +0800

    chore:Return and handle an error

--- hello/hello.go
+++ hello/hello.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"log"
 
 	"github.com/wwbab/Go_Documentation/greetings"
 	"rsc.io/quote"
@@ -10,7 +11,22 @@ import (
 func main() {
 	fmt.Println("Hello, World!")
 	fmt.Println(quote.Go())
-	// Get a greeting message and print it.
-	message := greetings.Hello("Jisidan")
+
+	// Set properties of the predefined Logger, including
+	// the log entry prefix and a flag to disable printing
+	// the time, source file, and line number.
+	log.SetPrefix("greetings: ")
+	log.SetFlags(0)
+
+	// Request a greeting message.
+	message, err:= greetings.Hello("")
+	// If an error was returned, print it to the console and 
+	// exit the program.
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// If no error was returned, print the returned message
+	//to the console.
 	fmt.Println(message)
 }

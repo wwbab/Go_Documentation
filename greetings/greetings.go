144113da3fd67f9fe3f2cddf9d317650962dd46b
wuwenbin <3303274053@qq.com>
2025-03-11 15:33:09 +0800

    test:Add greetings_test.go Add a test for the Hello function in the greetings.go

--- greetings/greetings.go
+++ greetings/greetings.go
@@ -15,6 +15,7 @@ func Hello(name string) (string, error) {
 	// If a name was received, return a value that embeds the name
 	// in a greeting message.
 	message := fmt.Sprintf(randomFormat(), name)
+	// message := fmt.Sprintf(randomFormat())
 	return message, nil
 }
 
